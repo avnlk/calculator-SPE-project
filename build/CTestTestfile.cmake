b7f99b9d0727c3a4dd6973f04c9648def4328e3d
Lokesh Aravapalli <lokesh1.aravapalli@gmail.com>
2025-10-10 01:47:53 +0530

    Remove build artifacts and add .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lokesh/Desktop/calculator_SPE_project
-# Build directory: /home/lokesh/Desktop/calculator_SPE_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(CalculatorTests "/home/lokesh/Desktop/calculator_SPE_project/build/runTests")
-set_tests_properties(CalculatorTests PROPERTIES  _BACKTRACE_TRIPLES "/home/lokesh/Desktop/calculator_SPE_project/CMakeLists.txt;31;add_test;/home/lokesh/Desktop/calculator_SPE_project/CMakeLists.txt;0;")
